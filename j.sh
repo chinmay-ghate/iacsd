9ab20052b182606c4fd21eb4045f6bc249d611a4
Chinmay Ghate <102283051+chinmay-ghate@users.noreply.github.com>
2023-06-20 22:28:01 +0530

    Update j.sh

--- j.sh
+++ j.sh
@@ -1,4 +1,15 @@
-#!/usr/bin/bash
-echo "my first script"
-whoami
+apt-get update -y
+apt-get upgrade -y
+apt-get install gnupg -y
+apt-get install default-jre -y
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
+    /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+    https://pkg.jenkins.io/debian binary/ | sudo tee \
+    /etc/apt/sources.list.d/jenkins.list > /dev/null
+apt update -y
+apt-get install jenkins -y
+sudo usermod -a -G root jenkins
+systemctl status jenkins
+
 
